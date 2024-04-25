6ee5e08d341206d377d97feb8bf8fda4229cabe6
sabelapereez <sabelapereez@gmail.com>
2024-04-25 18:06:41 +0200

    Prove

--- exchange.c
+++ exchange.c
@@ -2,23 +2,22 @@
 #include <stdio.h>
 #include "dynamicvector.h"
 
-//Adapt to dynamic vector
-
 /*Returns change in infinite coins mode*/
 int iexchange(int x, vectorD values, vectorD solution){
     unsigned long int n = VectorSize(values);
     for (int i = 0; i < n; i++) FillVector(&solution, i, 0);
     int i = 0, suma = 0;
     while (suma < x && i < n)
-        if (suma + valor[i]<=x){
-            solucion[i]++;
+        if (suma + Return(values, i) <= x){
+            TELEMENT aux = Return(solution, i);
+            FillVector(&solution, i, aux + 1);
             suma+=valor[i];
         }
-    else
-        i++;
-    if (suma==x) return 1;
+        else
+            i++;
+    if (suma == x) return 1;
     else{
-        for (int i=0;i<n;i++) solucion[i]=0;
+        for (int i = 0; i < n; i++) FillVector(&solution, i, 0);
         return 0;
     }
 }
--- exchange.h
+++ exchange.h
@@ -1,11 +1,14 @@
 #ifndef EXCHANGE_H
 #define EXCHANGE_H
 
+#include "dynamicvector.h"
+
 /**
  * Returns change in infinite coins mode.
  *
  * @param x amount to be exchanged.
- * @param value
+ * @param value vector with the valid values
+ * @param value vector with the returned coins
  * 
  */
 int iexchange(int x, vectorD values, vectorD solution);
